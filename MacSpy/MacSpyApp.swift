788d198db56a606f5a59fcddaba14ddf7eb38ca8
Crisp <tylykik@gmail.com>
2026-06-13 14:25:35 +0800

    Initial Commit

---
+++ MacSpy/MacSpyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacSpyApp.swift
+//  MacSpy
+//
+//  Created by Crisp on 2026/6/13.
+//
+
+import SwiftUI
+
+@main
+struct MacSpyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
